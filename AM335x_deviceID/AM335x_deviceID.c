fb7311f2d2fed96c79dc17539d17bb805611361e
Manu T. Sreedharan <manu.t.sree@gmail.com>
2019-05-19 22:05:58 +0200

    AM335x_deviceID

--- AM335x_deviceID/AM335x_deviceID.c
+++ AM335x_deviceID/AM335x_deviceID.c
@@ -8,8 +8,13 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#define CONTROL_MODULE_BASE     0x44E10000
-#define CONTROL_MODULE_SIZE     0x1000
+/* SPRUH73P – October 2011 – Revised March 2017
+ * table 2-2 page 180 */
+#define CONTROL_MODULE_BASE             0x44E10000
+#define CONTROL_MODULE_SIZE             0x1000
+#define CONTROL_REG_DEVICE_ID_OFFSET    0x600
+
+#define ERROR_MESSAGE_LEN       256
 
 #define DEBUG(x)    // x
 
@@ -20,31 +25,31 @@ void* Osal_mmap (void *ptr, uint32_t size) {
     int32_t     mmap_fd;
     int32_t     pageSize;
     void*       mmapBaseAddress;
-    
+
     /* Get the page size. */
     pageSize = sysconf(_SC_PAGE_SIZE);
     if (pageSize <= 0) {
-        printf("Osal_mmap(): PageSize is not correct: %d , errno: %d\n", pageSize, errno);
+        perror("Osal_mmap(): sysconf(_SC_PAGE_SIZE) failed");
         return NULL;
     }
-    
+
     /* Ensure block size and physical addresses are aligned to page size. */
     if ((size % pageSize) || ((uintptr_t)ptr % pageSize)) {
         printf("Osal_mmap(): Alignment error! (size mod page=%u, ptr mod page=%u\n", 
                (size % pageSize), (uint32_t)((uintptr_t)ptr % pageSize));
         return NULL;
     }
-    
+
     /* Open the /dev/mem file - for non-cachable memroy */
     if ((mmap_fd = open("/dev/mem", (O_RDWR | O_SYNC))) < 0) {
-        printf("Osal_mmap(): Could not open device /dev/mem\n");
+        perror("Osal_mmap(): Could not open device /dev/mem");
         return NULL;
     }
 
     /* Create a mapping of the physical address. */
     mmapBaseAddress = mmap(0, size, (PROT_READ|PROT_WRITE|PROT_EXEC), MAP_SHARED|MAP_LOCKED, mmap_fd, (off_t)ptr);
     if(mmapBaseAddress == MAP_FAILED) {
-        printf("Osal_mmap() failure : MAP_FAILED\n");
+        perror("Osal_mmap() mmap failure");
         close(mmap_fd);
         return NULL;
     }
@@ -60,29 +65,32 @@ void* Osal_mmap (void *ptr, uint32_t size) {
 /* Unmap a memory range */
 int Osal_munmap (void * ptr, uint32_t size) {
     DEBUG(printf("Osal_munmap(): ptr=%p, size=%u\n",ptr, size));
-    
+
+    char errMsg[ERROR_MESSAGE_LEN] = {0};
     if (munmap(ptr, size) == 0) {
         return 0;
     } else {
-        printf("ERROR: Osal_munmap failed for address: %p, size=%u. errno: %d", ptr, size, errno);
+        sprintf(errMsg, "ERROR: munmap(%p, %u) failed", ptr, size);
+        perror(errMsg);
         return errno;
     }
 }
 
 void AM335x_dumpDeviceID(uint32_t deviceID) {
     uint8_t devRev;
-    
+    printf("Device_ID:          0x%0x\n", deviceID);
+
     devRev = ((deviceID & 0xf0000000) >> 28);
     switch(devRev)
     {
         case 0b0000:
-            printf("Silicon Revision 1.0\n");
+            printf("Silicon Revision:   1.0\n");
             break;
         case 0b0001:
-            printf("Silicon Revision 2.0\n");
+            printf("Silicon Revision:   2.0\n");
             break;
         case 0b0010:
-            printf("Silicon Revision 2.1\n");
+            printf("Silicon Revision:   2.1\n");
             break;
         default:
             printf("Unidenfied Silicon Revision\n");
@@ -91,27 +99,29 @@ void AM335x_dumpDeviceID(uint32_t deviceID) {
 }
 
 int main(int argc, char **argv) {
-    printf("Hello world! This program prints the AM335x SoC Silicon Revision on your beaglebone black\n");
-    
+    printf("Hello world!\n");
+    printf("This program prints the AM335x SoC Silicon Revision on your beaglebone black\n");
+    printf("****************************************************************************\n");
+
     void* cmVirtAddr = NULL;
     int ret = 0;
     uint32_t deviceID;
-    
+
     cmVirtAddr = Osal_mmap((void*)CONTROL_MODULE_BASE, CONTROL_MODULE_SIZE);
     if(cmVirtAddr == NULL) {
         printf("Osal_mmap() failed! exiting the program\n");
         exit(-1);
     }
-    
-    deviceID = *(uint32_t*)(cmVirtAddr + 0x600);
-    
-    printf("Device_ID:              0x%0x\n", deviceID);
+
+    deviceID = *(uint32_t*)(cmVirtAddr + CONTROL_REG_DEVICE_ID_OFFSET);
+
     AM335x_dumpDeviceID(deviceID);
-    
+
     ret = Osal_munmap(cmVirtAddr, CONTROL_MODULE_SIZE);
     if(ret != 0){
         printf("Osal_munmap() failed! returnn value is %d\n", ret);
     }
-    
+
+    printf("****************************************************************************\n");
     return 0;
 }
--- AM335x_deviceID/README.md
+++ AM335x_deviceID/README.md
@@ -9,3 +9,9 @@ SDK path is /opt/ti/ti-processor-sdk-linux-am335x-evm-05.03.00.07.
 ./build.sh produces an executable AM335x_deviceID that can be executed on the
 beaglebone black. This program should be executed with super user previlages.
 
+steps to run:
+
+1. ./build.sh
+2. scp AM335x_deviceID debian@beaglebone:
+3. ssh debian@beaglebone
+4. sudo ./AM335x_deviceID
